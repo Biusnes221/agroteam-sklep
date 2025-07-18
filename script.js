e64908925136d47bbee6ba264f714b6d668db78f
Biusnes221 <wojtektomczak889@gmail.com>
2025-07-18 03:31:41 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 
-function zaplac(ranga) {
-  document.querySelector('#platnosc').style.display = 'block';
-  window.scrollTo(0, document.body.scrollHeight);
-}
+document.getElementById("purchaseForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  const modal = document.getElementById("blikModal");
+  modal.style.display = "block";
+  document.getElementById("selectedPrice").textContent = "Wybrana ranga (kwota podana w tytule)";
+});
+
+document.querySelector(".close").onclick = function () {
+  document.getElementById("blikModal").style.display = "none";
+};
+
+window.onclick = function (event) {
+  const modal = document.getElementById("blikModal");
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
